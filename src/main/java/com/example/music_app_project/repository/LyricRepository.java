afca2783f874fec4456bd9c619898a0d8aeac3da
T T <hdang4747@gmail.com>
2025-07-14 19:15:57 +0700

    fix lyrics and user controller

--- src/main/java/com/example/music_app_project/repository/LyricRepository.java
+++ src/main/java/com/example/music_app_project/repository/LyricRepository.java
@@ -2,8 +2,10 @@ package com.example.music_app_project.repository;
 
 import com.example.music_app_project.model.Lyric;
 import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
+import java.util.*;
 
 public interface LyricRepository extends JpaRepository<Lyric, Integer> {
-    List<Lyric> findBySongSongID(Integer songID); // Optional: find all lyrics for a song
+    List<Lyric> findBySong_SongID(Integer songID);
+
+    Optional<Lyric> findFirstBySong_SongID(Integer songID);// Optional: find all lyrics for a song
 }
--- src/main/java/com/example/music_app_project/repository/UserRepository.java
+++ src/main/java/com/example/music_app_project/repository/UserRepository.java
@@ -1,10 +1,12 @@
 package com.example.music_app_project.repository;
 
+import java.util.*;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.example.music_app_project.model.User;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
-    boolean existsByUsername(String username);
+    Optional<User> findByUsername(String username);
 
-    boolean existsByEmail(String email);
+    Optional<User> findByEmail(String email);
 }
